3c3cb905dd7b0ccd1749ab47e38e4558d62c8bdc
eduardokoch <eduardo.ekoch@gmail.com>
2018-06-25 13:08:11 -0300

    Inserir projeto cards

---
+++ card.rb
@@ -0,0 +1,38 @@
+class Card
+  attr_accessor :portugues
+  attr_accessor :ingles
+  attr_accessor :imagem
+  # quando tiver reader E writer, pode usar attr_accessor
+
+  def initialize(port, ing = "")
+    @portugues = port
+    @ingles = ing
+    @imagem = "nenhuma"
+  end
+
+  def imprimir()
+    puts "Card: " + to_s()
+  end
+
+  # em Ruby usamos o snake_case para métodos
+  def to_s()
+    "#{portugues()} => #{ingles()}"
+  end
+
+  def ==(outro_card)
+    portugues() == outro_card.portugues() &&
+    ingles() == outro_card.ingles()
+  end
+
+  def include?(texto)
+    # true E false => false
+    # true E true => true
+    # false E false => false
+    # true OU false => true
+    # true OU true => true
+    # false OU false => false
+    # E => &&; OU => ||
+    portugues().include?(texto) || ingles().include?(texto)
+  end
+
+end
