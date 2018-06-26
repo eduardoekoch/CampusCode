d2d5d8df81931da43ca13c39f0e910f84715b4c7
eduardokoch <eduardo.ekoch@gmail.com>
2018-06-25 23:08:58 -0300

    Acrescimo de busca

--- ImersaoFerias/cards.rb
+++ ImersaoFerias/cards.rb
@@ -1,5 +1,7 @@
 require_relative 'card'
 
+cards = []
+
 def menu()
   puts "Digite a opção desejada"
   puts "[1] Inserir um novo card"
@@ -12,42 +14,28 @@ def menu()
 end
 
 def inserir_card(cards)
+  card = {}
   puts 'Insira uma expressão em Português:'
-  portugues = gets.chomp
+  card[:portugues] = gets.chomp.downcase
   puts 'Insira uma expressão em Ingles:'
-  ingles = gets.chomp
-  card = Card.new(portugues, ingles)
-
-  card_existe = false
-
+  card[:ingles] = gets.chomp.downcase
+  cards << card
   # Bonus/Desafio: estudar e usar o método select
-  cards.each do |c|
-    if c == card
-      puts 'Esse card já existe!'
-      card_existe = true
-    end
-  end
+  #cards.each do |c|
+  #  if c == card
+  #    puts 'Esse card já existe!'
+  #    card_existe = true
+  #  end
+  #end
 
-  if card_existe == false
-    cards << card
-    puts "Você inseriu o card: #{card}"
-  end
-  puts
-  return card # o 'return' é opcional!
+  #if card_existe == false
+  #  cards << card
+  #  puts "Você inseriu o card: #{card}"
+  #end
+  #puts
+  #return card # o 'return' é opcional!
 end
 
-def imprimir_cards(cartoes)
-  puts "Cards Cadastrados:"
-  puts
-  cartoes.each do |cartao|
-    cartao.imprimir()
-  end
-  puts
-end
-
-
-
-cards = []
 puts 'Bem vindo ao Cards System'
 opcao = menu()
 
@@ -57,27 +45,53 @@ while opcao != 4
     c = inserir_card(cards)
 
   elsif opcao == 2
-
-    imprimir_cards(cards)
+    puts "Cards Cadastrados:"
+    cards.each do|card|
+    print card
+    puts
+  end
 
   elsif opcao == 3
-    print "Informe o texto que está procurando: "
-    texto_busca = gets.chomp
+    puts "[1] se o texto a ser procurado está em inglês"
+    puts "[2] se o texto a ser procurado está em português"
+    texto_busca_tipo = gets.to_i
 
-    cards.each do |c|
-      if c[:portugues].include?(texto_busca)
-        puts "Card Encontrado: #{c}"
-      end
+    if (texto_busca_tipo == 1)
+      puts("Insira a palavra em inglês")
+      texto_busca_ingles = gets.chomp.downcase
+      puts ("A palavra a ser buscada é #{texto_busca_ingles}")
+      #ACHOU = 0
+      #card = {}
+        cards.each do |card|
+          if card[:ingles].include? texto_busca_ingles
+            #ACHOU = 1
+            #card_encontrado = "#{card}"
+            puts
+            print "Card encontrado: #{card}"
+            puts
+          else
+            #ACHOU = 0
+            puts
+            print "Card não encontrado en-br"
+            puts
+          end
+        end
+        #print "Card encontrado: #{card_encontrado}"
+    elsif (texto_busca_tipo == 2)
+      puts "Insira a plavra em português"
+      texto_busca_portugues = gets.chomp.downcase
+        cards.each do |card|
+          if card[:portugues].include? texto_busca_portugues
+            puts
+            print "Card encontrado: #{card}"
+            puts
+          else
+          puts "Card não encontrado pt-br"
+          end
+        end
     end
-
-    # XX cards encontrados
-
-  else
-    puts 'Entre com uma opcao válida'
-    puts
   end
   opcao = menu()
 end
 
 puts 'Obrigado por usar o Cards System!'
-
